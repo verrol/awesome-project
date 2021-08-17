c1f54141bcca4e4960570f231eed7a4c5fd50215
Verrol Adams <verrol@lorrev.org>
2021-08-17 10:25:18 -0400

    missed in the last update, updated structure

---
+++ proj1/ex03/cmd/app2/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/verrol/awesome-project/ex03/internal/cmd/app2"
+
+func main() {
+	app2.Run()
+}
---
+++ proj2/cmd/myapp2/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/verrol/awesome-project/ex03/internal/cmd/app1"
+
+func main(){
+    app.Run()
+}
